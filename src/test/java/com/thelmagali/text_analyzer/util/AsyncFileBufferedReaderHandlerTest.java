184b4d73a4689167c6c191aff8ad8d811e2e8ce5
Thelma Balbuena <thelmabalbuena@gmail.com>
2023-06-24 17:07:44 -0400

    Added more test cases

--- src/test/java/com/thelmagali/text_analyzer/util/AsyncFileBufferedReaderHandlerTest.java
+++ src/test/java/com/thelmagali/text_analyzer/util/AsyncFileBufferedReaderHandlerTest.java
@@ -21,17 +21,24 @@ public class AsyncFileBufferedReaderHandlerTest {
 
   private FileSystem fileSystem = new FileSystemImpl((VertxInternal) vertx);
 
+  String[] lines = new String[]{"Hello world", "another line", "another one"};
+
   @Test
-  public void testAsyncFileHandler() throws InterruptedException {
-    Promise<Set<String>> resultPromise = Promise.promise();
-    AsyncFileBufferedReaderHandler myHandler = new AsyncFileBufferedReaderHandler(resultPromise, 1);
-    String[] lines = new String[]{"Hello world", "another line"};
+  void testWithDifferentBufferSizes() throws InterruptedException {
     String fileName = writeFile(lines);
+    testAsyncFileHandler(1, fileName);
+    testAsyncFileHandler(2, fileName);
+    testAsyncFileHandler(10, fileName);
+    testAsyncFileHandler(4096, fileName);
+  }
+
+  private void testAsyncFileHandler(int bufferSize, String fileName) throws InterruptedException {
+    Promise<Set<String>> resultPromise = Promise.promise();
+    AsyncFileBufferedReaderHandler myHandler = new AsyncFileBufferedReaderHandler(resultPromise, bufferSize);
     fileSystem.open(fileName, new OpenOptions().setRead(true), myHandler);
 
-    Thread.sleep(200); // waiting for the future
+    Thread.sleep(1000); // waiting for the future
     resultPromise.future().onSuccess(result -> {
-      System.out.println(result);
       assert (result.size() == lines.length);
       assert (new ArrayList<>(result).containsAll(Arrays.stream(lines).collect(Collectors.toList())));
     });
@@ -41,7 +48,7 @@ public class AsyncFileBufferedReaderHandlerTest {
     var fileName = fileSystem.createTempFileBlocking("prefix", ".txt");
     StringJoiner joiner = new StringJoiner("\n");
     Arrays.stream(lines).forEach(joiner::add);
-    Buffer buffer = Buffer.buffer(joiner.toString());
+    Buffer buffer = Buffer.buffer(joiner + "\n");
     fileSystem.writeFileBlocking(fileName, buffer);
     return fileName;
   }
